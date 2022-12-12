dcba8cf707afde4123bcba007c7ddf87b9eea3ca
hussy.levente <hussy.levente@119-12>
2022-12-12 15:55:29 +0100

    Add project files.

---
+++ projekt_feladat_hussy_levente_11a/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("1. Összeadás\n2. Kivonás\n3. Szorzás\n4. Osztás\n\nVálasszon menüpontot(1-4):");
+int ValasztottSzam = int.Parse(Console.ReadLine());
+
+while (ValasztottSzam !< 5)
+{
+    if(ValasztottSzam > 4)
+    {
+        Console.WriteLine("Rossz számot adott meg.");
+        break;
+    }
+
+    if(ValasztottSzam == 1)
+    {
+
+    }
+}
